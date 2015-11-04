6a55ce16d5ec2d831fbd96e97d9b75b3beafd4b4
Jack Dalton <jackdalton00@gmail.com>
2015-11-04 02:08:11 +0000

    Added tests for minified version

--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
 var assert = require("assert");
 var Engine2D = require("../src/engine2d");
+var Engine2DM = require("../prod/engine2d.min");
 
 describe("Engine2D", function() {
     describe("#GameScene()", function() {
@@ -21,6 +22,29 @@ describe("Engine2D", function() {
     describe("#Circle()", function() {
         it("should be object", function() {
             assert.equal(typeof new Engine2D.Circle(), "object");
+        });
+    });
+});
+describe("Engine2D minified", function() {
+    describe("#GameScene()", function() {
+        it("should be object", function() {
+            assert.equal(typeof new Engine2DM.GameScene(), "object");
+        });
+        it("should have an array of objects named `objects`", function() {
+            assert.equal(typeof new Engine2DM.GameScene().objects, "object");
+        });
+        it("should not have a public function named `isValidID`", function() {
+            assert.equal(typeof new Engine2DM.GameScene().isValidID, "undefined");
+        });
+    });
+    describe("#Rect()", function() {
+        it("should be object", function() {
+            assert.equal(typeof new Engine2DM.Rect(), "object");
+        });
+    });
+    describe("#Circle()", function() {
+        it("should be object", function() {
+            assert.equal(typeof new Engine2DM.Circle(), "object");
         });
     });
 });
